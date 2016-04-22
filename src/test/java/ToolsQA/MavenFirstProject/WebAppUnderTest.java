6f623df0efe9f44732a6fad02912af129b3fbac7
Ade7Wale <Ade7Wale@gmail.com>
2016-04-22 16:42:34 +0100

    Commiting again - made change to WebAppUnderTest.java

--- src/test/java/ToolsQA/MavenFirstProject/WebAppUnderTest.java
+++ src/test/java/ToolsQA/MavenFirstProject/WebAppUnderTest.java
@@ -1,7 +1,9 @@
 package ToolsQA.MavenFirstProject;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
@@ -12,14 +14,22 @@ public class WebAppUnderTest
 	public void NavigateToURL() throws InterruptedException {
 		
 		WebDriver driver  = new FirefoxDriver();
+		WebElement element;
+		
 		System.out.println("The Browser is open");
 		
-		driver.get("http://www.bbc.com");
+		driver.get("http://www.gmail.com");
 		System.out.println("The URL is entered");
 		System.out.println("...waiting for 5 seconds before closing Browser");
 		
 		Thread.sleep(5000L);
 		System.out.println("...waiting for 5 seconds");
+		
+		System.out.println("Finding Element & start entering txt");
+		//finding element for username txtbox
+		element =  driver.findElement(By.id("Email"));
+		//sending strings to element
+		element.sendKeys("Testing@email.com");
 		
 		driver.close();
 		System.out.println("The Browser is closed");
